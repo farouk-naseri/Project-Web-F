f7b403cefe138b9aa12d79e1ce5c6369c4df9d37
farouk-naseri <naseri.farouk.abderraouf@gmail.com>
2024-05-22 05:41:41 +0100

    section principal

--- js/script.js
+++ js/script.js
@@ -12,3 +12,36 @@ menubtn.addEventListener("click",function(){
     }
 })
 //end of SLIDE NAVBAR
+// start of formation part
+// Select all buttons with the class "formation"
+const buttons = document.querySelectorAll(".formation button");
+
+// Add event listener to each button
+buttons.forEach((button, index) => {
+    button.addEventListener("click", function() {
+        let page;
+        switch (index) {
+            case 0:
+                page = "section1.html";
+                break;
+            case 1:
+                page = "section2.html";
+                break;
+            case 2:
+                page = "section3.html";
+                break;
+            default:
+                page = "default.html"; 
+        }
+        window.location.href = page;
+    });
+});
+// end of formation paert
+// start of asid scroll efect
+const scrollEffects = document.querySelectorAll(".scroll-efect");
+
+scrollEffects.forEach((element, index) => {
+    const topPosition = index * 50; 
+    element.style.top = `${topPosition}px`;
+});
+// end of aside scroll efect
