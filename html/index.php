9fb87413307305c769df149f7da779b5aad31f59
yousfiNacerEl0 <yousfin027@gmail.com>
2024-05-29 09:27:23 +0100

    added comment section with php and a little change in section principale

--- html/index.php
+++ html/index.php
@@ -7,7 +7,7 @@
     <!-- icon links -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <!-- css link -->
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <!-- font link -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -21,40 +21,42 @@
         <!--  Comme titre le nom de l’école de formation ;
              Une image de fond. -->
             <i class='bx bx-menu'></i>
-            <img src="/tools/Batman.png" alt="">
+            <img src="../tools/Batman.png" alt="">
             <h2>3elemni <span>courses</span></h2>
             <ul>
                 <li><a href="sec1">services</a></li>
-                <li><a href="sec2">feedback</a></li>
+                <li><a href="#section2">feedback</a></li>
             </ul>
         </header>
         <!-- END OF HEADER s -->
 <!-- START OF NAV  -->
 <nav>
-    <div class="box principal">
-        <div><i class='bx bxs-school'></i><p>UMBB</p></div>
-        <div><p>Section one </p></div>
-        <div><i class='bx bxs-group'></i><p>group <span>10</span></p></div>
-    </div>
-    <div class="box secondair">
-        <div><i class='bx bxs-user'></i><p>Naseri Farouk</p></div>
-        <div>
-            <a href="mailto:naseri.farouk.abderraouf@gmail.com"><i class='bx bx-envelope'></i></a>
-            <p>naseri.farouk.abderraouf@gmail.com</p>
+    <div class="container">
+        <div class="box principal">
+            <div><i class='bx bxs-school'></i><p>UMBB</p></div>
+            <div style="color:#2D222F;"><p>Section one </p></div>
+            <div style="color: #2D222F ;" ><i class='bx bxs-group' ></i><p>group <span>10</span></p></div>
         </div>
-    </div>
-    <div class="box secondair">
-        <div><i class='bx bxs-user'></i><p>Yousfi Nacer</p></div>
-        <div>
-            <a href="mailto:yousfin027@gmail.com"><i class='bx bx-envelope'></i></a>
-            <p>yousfin027@gmail.com</p>
+        <div class="box secondair">
+            <div class="name"><i class='bx bxs-user'></i><p>Naseri Farouk</p></div>
+            <div class="eemail">
+                <a href="mailto:naseri.farouk.abderraouf@gmail.com"><i class='bx bx-envelope'></i></a>
+                <p>naseri.farouk.abderraouf@gmail.com</p>
+            </div>
         </div>
-    </div>
-    <div class="box secondair">
-        <div><i class='bx bxs-user'></i><p>Bouras Ayoub</p></div>
-        <div>
-            <a href="mailto:bou123ywi@gmail.com"><i class='bx bx-envelope'></i></a>
-            <p>bou123ywi@gmail.com</p>
+        <div class="box secondair">
+            <div class="name"><i class='bx bxs-user'></i><p>Yousfi Nacer</p></div>
+            <div class="eemail">
+                <a href="mailto:yousfin027@gmail.com"><i class='bx bx-envelope'></i></a>
+                <p>yousfin027@gmail.com</p>
+            </div>
+        </div>
+        <div class="box secondair">
+            <div class="name"><i class='bx bxs-user'></i><p>Bouras Ayoub</p></div>
+            <div class="eemail">
+                <a href="mailto:bou123ywi@gmail.com"><i class='bx bx-envelope'></i></a>
+                <p>bourasayoub41@gmail.com</p>
+            </div>
         </div>
     </div>
 </nav>
@@ -69,10 +71,10 @@
                     <h2>why 3elemni courses</h2>
                     <p>Our website is dedicated to empowering our customers with the knowledge they needto excel in their chosen domains, using the latest technology. We aim to provide comprehensiveresources and insights that enable our users to achieve professional mastery and generate income from their expertise.  </p>
                 </div>
-                <h2>Nos Formations</h2>
             </div>
-
-<!-- start of formation part -->
+            
+            <!-- start of formation part -->
+            <h2>Nos Formations</h2>
             <div class="formation-part">
                 <div class="formation">
                     <article>
@@ -128,5 +130,64 @@
         </div>
     </section>
     <!-- END OF PRINCIPAL SECTION  -->
+    <!-- START OF SECTION 2 -->
+    <section id="section2">
+        <div class="container">
+                <form  class="comments" method="post" >
+                    <div>
+                        <input type="email" id="email" name="email" placeholder="Email-@-adress" required >
+                        <span></span>
+                    </div>
+                        <textarea name="comments" id="comments-paragraph" placeholder="Share ur opinion..." required></textarea>
+                        <input type="submit" value="envoyer" name="envoyer" id="envoyer">
+                </form>
+                <?php
+                    //connextion a la base de donnees
+                        $db = new PDO('mysql:host=localhost;dbname=comptes',"root","");
+                        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    //insertion dans la base de donnees
+                        if(!empty($_POST["envoyer"])){
+                            $email = $_POST["email"];
+                            $commentaire = $_POST["comments"];
+                            
+                            $request = $db->prepare("INSERT INTO comptes (emails,commentaire) VALUE (:email,:commentaire)");
+                            $request->bindValue(":email",$email);
+                            $request->bindValue(":commentaire",$commentaire);
+
+                            $result = $request->execute();
+                        }
+                    ?>
+            <div class="display-comments">
+                <div class="titre">
+                    Commentaire
+                </div>
+               <?php 
+                //recuperation de la base de donnees
+                   
+                    $request = $db->query("SELECT * FROM comptes") ;
+                    while($result = $request->fetch()){
+                        echo '<div class="comment-style">';
+                            echo '<div class="email-part">';
+                                echo '<i class="bx bx-user"></i>';
+                                echo $result["emails"];
+                            echo '</div>';
+                            echo '<div class="comment-part">';
+                                echo $result["commentaire"];
+                            echo '</div>';
+                            echo '<div class="like-part">';
+                               echo '<i class="bx bxs-heart"></i><span class=""like-count></span>';
+
+                               echo '<i class="bx bxs-dislike" ></i><span class=""like-count></span>';
+                            echo '</div>';
+
+                        echo '</div>';
+                    } 
+                  
+               ?>
+            </div>
+        </div>
+    </section>
+    <!-- END OF SECTOIN 2  -->
+   
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -45,3 +45,45 @@ scrollEffects.forEach((element, index) => {
     element.style.top = `${topPosition}px`;
 });
 // end of aside scroll efect
+
+//like
+const likebtns = document.querySelectorAll(".bxs-heart");
+//when the user like
+likebtns.forEach(likebtn => {
+    likebtn.addEventListener("click", () => {
+        likebtn.classList.add("likeEffect");
+        likebtn.classList.remove("rm-likeEffect");
+        likebtn.nextElementSibling.textContent = "1";
+        likebtn.nextElementSibling.style.color = "red";
+    });
+});
+//when the user remouve the like
+likebtns.forEach(likebtn => {
+    likebtn.addEventListener("dblclick", () => {
+        likebtn.classList.remove("likeEffect");
+        likebtn.classList.add("rm-likeEffect");
+        likebtn.nextElementSibling.textContent = "";
+        likebtn.style.color = "gray";
+    });
+});
+
+const unlikebtns = document.querySelectorAll(".bxs-dislike");
+//when the user dislike 
+unlikebtns.forEach(unlikebtn => {
+    unlikebtn.addEventListener("click", () => {
+        unlikebtn.style.color = "black";
+        unlikebtn.nextElementSibling.textContent ="1";
+        unlikebtn.nextElementSibling.style.color ="black";
+    });
+});
+//when the user remouve the dislike
+unlikebtns.forEach(unlikebtn => {
+    unlikebtn.addEventListener("dblclick", () => {
+        unlikebtn.style.color = "black";
+        unlikebtn.nextElementSibling.textContent ="";
+        unlikebtn.style.color ="gray";
+    });
+});
+
+
+
